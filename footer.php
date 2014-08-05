bdabd5ad1d8e6b65cc0c9a8348f89c46deefb7d0
Administrator <Administrator@IK100Kwon>
2014-08-05 16:57:40 +0800

    Fix the paypal account issue and background position.

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
   <div class="paypal-donate">
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
       <input type="hidden" name="cmd" value="_donations">
-      <input type="hidden" name="business" value="paypal@enooxne.ch">
+      <input type="hidden" name="business" value="paypal@enoxone.ch">
       <input type="hidden" name="lc" value="US">
       <input type="hidden" name="no_note" value="0">
       <input type="hidden" name="currency_code" value="USD">
